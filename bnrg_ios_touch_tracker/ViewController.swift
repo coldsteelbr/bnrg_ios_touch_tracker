54e6d53ba61fb7070fc39f3ca8a475d16154f475
Roman <cold.steel.br@gmail.com>
2018-05-31 17:22:30 +0700

    Initial Commit

---
+++ bnrg_ios_touch_tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  bnrg_ios_touch_tracker
+//
+//  Created by Roman Brazhnikov on 31.05.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
