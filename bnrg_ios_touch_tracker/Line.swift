7c12fef3dcc112587b8dae1de10699a27f7f0180
Roman <cold.steel.br@gmail.com>
2018-06-01 00:13:37 +0700

    One touch handled

---
+++ bnrg_ios_touch_tracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  bnrg_ios_touch_tracker
+//
+//  Created by Roman Brazhnikov on 31.05.2018.
+//  Copyright © 2018 Roman Brazhnikov. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line{
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
